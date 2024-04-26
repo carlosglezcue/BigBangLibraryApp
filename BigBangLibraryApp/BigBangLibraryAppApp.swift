651f2b97c40627a68a0cb6bba9a2b22fd5adab2f
Carlos González Cuesta <carlos.gonzalezc@babelgroup.com>
2024-04-26 19:50:41 +0200

    Initial Commit

---
+++ BigBangLibraryApp/BigBangLibraryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBangLibraryAppApp.swift
+//  BigBangLibraryApp
+//
+//  Created by carlos.gonzalez.local on 26/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BigBangLibraryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
